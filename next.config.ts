2c10ab2aa128c2352a0eb4be67894ddc03500e2f
Nicolas Brown <nclsbrwn@gmail.com>
2024-12-23 22:23:46 -0500

    project setup

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  experimental: { esmExternals: true },
+  webpack: function (config, { isServer, dev }) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      syncWebAssembly: true,
+    };
+
+    // TODO: improve this function -> track https://github.com/vercel/next.js/issues/25852
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? "../static/wasm/[modulehash].wasm"
+        : "static/wasm/[modulehash].wasm";
+
+    return config;
+  },
 };
 
 export default nextConfig;
